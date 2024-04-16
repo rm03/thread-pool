07d066355e121328f4adf7cc431b3f2a55cf1492
Michael <michael.clyde.lu@gmail.com>
2024-04-16 02:10:42 -0400

    Added starter code in header

--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
 #include "ts_queue.hpp"
+#include <vector> 
+
+class thread_pool {
+    public:
+        thread_pool(const uint8_t);
+        ~thread_pool(); 
+    private: 
+        std::vector<ts_queue> queues; 
+        std::vector<std::thread> threads;
+        const uint8_t num_threads; 
+}
 
 #include "thread_pool.tpp"
--- thread_pool.tpp
+++ thread_pool.tpp
@@ -1,3 +1,2 @@
 #include "thread_pool.hpp"
 
-
