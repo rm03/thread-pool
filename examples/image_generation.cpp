ed541d081fe1970a2c081a971cda30d628b47fdf
Rohan Moniz <60864468+rm03@users.noreply.github.com>
2024-04-25 00:58:28 -0400

    update image generation example

--- examples/image_generation.cpp
+++ examples/image_generation.cpp
@@ -4,50 +4,83 @@
 #define STB_PERLIN_IMPLEMENTATION
 #include "stb_perlin.h"
 
+#include <chrono>
 #include <vector>
 
-void generatePerlinNoiseImage(const char *filename, int width, int height, unsigned int seed) {
-    std::vector<unsigned char> image(width * height * 3);
-
-    float scale = 0.1;
+void generatePerlinNoiseImage(std::vector<unsigned char> &image, int width, int height, unsigned int seed) {
+    float scale = 0.006;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            float n = stb_perlin_noise3(x * scale, y * scale, seed, 0, 0, 0);
-            unsigned char value = static_cast<unsigned char>((n + 1.0) * 0.5 * 255);
+            float n = stb_perlin_noise3_seed((x + (seed % 100)) * scale, y * scale, 0, 0, 0, 0, seed);
             int index = (y * width + x) * 3;
-            image[index] = value;                   // Red channel
-            image[index + 1] = 255 - value;         // Green channel
-            image[index + 2] = (value + 128) % 256; // Blue channel
+            if (n < 0) {
+                image[index] = 0;
+                image[index + 1] = 0;
+                image[index + 2] = 0.5 + 0.5 * (n + 1) * 255;
+            } else if (n < 0.4) {
+                image[index] = 0;
+                image[index + 1] = 0.5 + 0.5 * (n + 1) * 255;
+                image[index + 2] = 0;
+            } else {
+                image[index] = 255 * (0.7 + (0.3 * n));
+                image[index + 1] = 255 * (0.7 + (0.3 * n));
+                image[index + 2] = 255 * (0.7 + (0.3 * n));
+            }
         }
     }
-
-    stbi_write_png(filename, width, height, 3, image.data(), width * 3);
 }
 
-void generatePerlinNoiseImage(std::vector<unsigned char> &image, int width, int task_height, unsigned int seed) {
-    float scale = 0.1;
+void generatePerlinNoiseImageThreaded(std::vector<unsigned char> &image, int width, int task_height, unsigned int seed) {
+    float scale = 0.006;
     for (int x = 0; x < width; x++) {
-        float n = stb_perlin_noise3(x * scale, task_height * scale, seed, 0, 0, 0);
-        unsigned char value = static_cast<unsigned char>((n + 1.0) * 0.5 * 255);
+        float n = stb_perlin_noise3_seed(x * scale, task_height * scale, 0, 0, 0, 0, seed);
         int index = (task_height * width + x) * 3;
-        image[index] = value;                   // Red channel
-        image[index + 1] = 255 - value;         // Green channel
-        image[index + 2] = (value + 128) % 256; // Blue channel
+        if (n < 0) {
+            image[index] = 0;
+            image[index + 1] = 0;
+            image[index + 2] = 0.5 + 0.5 * (n + 1) * 255;
+        } else if (n < 0.4) {
+            image[index] = 0;
+            image[index + 1] = 0.5 + 0.5 * (n + 1) * 255;
+            image[index + 2] = 0;
+        } else {
+            image[index] = 255 * (0.7 + (0.3 * n));
+            image[index + 1] = 255 * (0.7 + (0.3 * n));
+            image[index + 2] = 255 * (0.7 + (0.3 * n));
+        }
     }
 }
 
 int main() {
-    
-    constexpr int WIDTH = 256;
-    constexpr int HEIGHT = 256;
-    std::vector<unsigned char> image(WIDTH * HEIGHT * 3);
-    thread_pool pool;
-    for (int i = 0; i < HEIGHT; i++) {
-        pool.enqueue_task([&]() {
-            generatePerlinNoiseImage(image, WIDTH, i, 0);
-        });
+    constexpr int WIDTH = 4096;
+    constexpr int HEIGHT = 4096;
+    std::vector<unsigned char> image(3 * WIDTH * HEIGHT);
+
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    auto seed = std::rand();
+
+    // multithreaded
+    auto start_threaded = std::chrono::high_resolution_clock::now();
+    {
+        thread_pool pool;
+        for (int i = 0; i < HEIGHT; i++) {
+            pool.enqueue_task([&, i]() {
+                generatePerlinNoiseImageThreaded(image, WIDTH, i, seed);
+            });
+        }
     }
-    
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    stbi_write_png("test.png", WIDTH, HEIGHT, 3, image.data(), WIDTH * 3);
+    auto end_threaded = std::chrono::high_resolution_clock::now();
+    stbi_write_png("image.png", WIDTH, HEIGHT, 3, image.data(), WIDTH * 3);
+
+    // single-threaded
+    auto start_nonthreaded = std::chrono::high_resolution_clock::now();
+    generatePerlinNoiseImage(image, WIDTH, HEIGHT, std::rand());
+    auto end_nonthreaded = std::chrono::high_resolution_clock::now();
+    stbi_write_png("image2.png", WIDTH, HEIGHT, 3, image.data(), WIDTH * 3);
+
+    auto duration_threaded = std::chrono::duration_cast<std::chrono::milliseconds>(end_threaded - start_threaded);
+    auto duration_nonthreaded = std::chrono::duration_cast<std::chrono::milliseconds>(end_nonthreaded - start_nonthreaded);
+
+    std::cout << "Threaded execution time: " << duration_threaded.count() << " ms\n";
+    std::cout << "Non-threaded execution time: " << duration_nonthreaded.count() << " ms\n";
 }
