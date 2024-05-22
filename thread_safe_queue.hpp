a24edcde0f66b8924064c212fee2eb917f501c84
Michael <michael.clyde.lu@gmail.com>
2024-05-22 16:25:19 -0500

    modified locks

--- thread_safe_queue.hpp
+++ thread_safe_queue.hpp
@@ -6,17 +6,17 @@ template <typename T>
 class thread_safe_queue {
 public:
     void push_front(T &&elt) {
-        std::scoped_lock lock(mutex);
+        std::lock_guard lock(mutex);
         queue.push_front(std::forward<T>(elt));
     }
 
     void push_back(T &&elt) {
-        std::scoped_lock lock(mutex);
+        std::lock_guard lock(mutex);
         queue.push_back(std::forward<T>(elt));
     }
 
     std::optional<T> pop_front() {
-        std::scoped_lock lock(mutex);
+        std::lock_guard lock(mutex);
         if (queue.empty()) {
             return std::nullopt;
         }
@@ -26,7 +26,7 @@ public:
     }
 
     std::optional<T> pop_back() {
-        std::scoped_lock lock(mutex);
+        std::lock_guard lock(mutex);
         if (queue.empty()) {
             return std::nullopt;
         }
@@ -36,7 +36,7 @@ public:
     }
 
     bool empty() {
-        std::scoped_lock lock(mutex);
+        std::lock_guard lock(mutex);
         return queue.empty();
     }
 
